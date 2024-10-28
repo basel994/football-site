77209b0a90be0bbbbbc5f3cde18ceda400375638
baselbalkes <baselbalkes77@gmail.com>
2024-10-28 14:39:57 +0300

    fix(api GET params: use promise)

--- src/app/api/news/[id]/route.ts
+++ src/app/api/news/[id]/route.ts
@@ -1,13 +1,17 @@
 import { sql } from "@vercel/postgres";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(  
-    {params}: {params: {id: string}}) {
+export async function GET( 
+    request: NextRequest, 
+    { params }: { params: Promise<{ id: string }> }) {
+        const headers = request.headers;
+        headers.get("Content-Type");
+        const id = (await params).id
         try {
             const fetchNewQuery = await sql `
-            SELECT * FROM sportNews WHERE id = ${parseInt(params.id)}
+            SELECT * FROM sportNews WHERE id = ${parseInt(id)}
             `;
-            return NextResponse.json({data: fetchNewQuery.rows[0]})
+            return NextResponse.json({data: fetchNewQuery.rows[0]});
         } catch ( error ) {
             console.log( error );
             return NextResponse.json({error: "فشل في تحميل الخبر"});
