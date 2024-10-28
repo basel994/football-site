2122f7e3235a9dc390fa9cf6efbcdf9b7de6ad9a
baselbalkes <baselbalkes77@gmail.com>
2024-10-28 13:54:41 +0300

    improve news style and add api for fetching new details

--- src/components/News/New/New.tsx
+++ src/components/News/New/New.tsx
@@ -1,20 +1,36 @@
 import { NewType } from "@/types/newType";
 import styles from "./new.module.css";
 import Image from "next/image";
+import { dateForm } from "@/functions/dateForm";
+import { truncateText } from "@/functions/truncateText";
+import CustomButton from "@/components/CustomButton/CustomButton";
+import Link from "next/link";
 
 export default function New( {newObject}: {newObject: NewType} ) {
+    const getStringDate = dateForm(newObject.created_at);
+    const getTruncatedText = truncateText(newObject.content, 15);
     return(
         <div className={styles.new}>
             <div>
                 <Image src={newObject.image} 
                 alt="" 
-                width={75} 
-                height={75} />
+                width={150} 
+                height={150} />
             </div>
             <div className={styles.details}>
-                <p>{new Date(newObject.created_at).toDateString()}</p>
+                <div className={styles.date}>
+                    <p>{getStringDate.stringDate}</p>
+                    <p>{getStringDate.stringTime}</p>
+                </div>
                 <h3>{newObject.title}</h3>
-                <p>{newObject.content}</p>
+                <p>{getTruncatedText}</p>
+                <div className={styles.more}>
+                    <Link href={`/news/${newObject.id}`}>
+                    <CustomButton title="التفــاصيل" 
+                    bg="black" 
+                    color="white" />
+                    </Link>
+                </div>
             </div>
         </div>
     );
---
+++ src/functions/dateForm.ts
@@ -0,0 +1,41 @@
+type StringDateType = {
+    stringDate: string;
+    stringTime: string;
+}
+export function dateForm(date: Date): StringDateType {
+    const toDate = new Date(date);
+    const stringTime = `
+    ${toDate.getMinutes()} : 
+    ${toDate.getHours()} 
+    ${toDate.getHours() < 12 ? "AM" : "PM"}
+    `;
+    const today = new Date();
+    const yesterday = new Date(today);
+    yesterday.setDate(today.getDate() - 1);
+    const tomorrow = new Date(today);
+    tomorrow.setDate(today.getDate() + 1);
+    if(toDate.toDateString() === today.toDateString()) {
+        return {
+            stringDate: "اليـوم", 
+            stringTime: stringTime, 
+        };
+    }
+    else if(toDate.toDateString() === yesterday.toDateString()) {
+        return {
+            stringDate: "أمس", 
+            stringTime: stringTime, 
+        };
+    }
+    else if(toDate.toDateString() === tomorrow.toDateString()) {
+        return {
+            stringDate: "غـداً", 
+            stringTime: stringTime, 
+        };
+    }
+    else {
+        return {
+            stringDate: `${toDate.getDay()} / ${toDate.getMonth()} / ${toDate.getFullYear()}`, 
+            stringTime: stringTime, 
+        };
+    }
+}
