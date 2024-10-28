489676187b5c324fd48780ea31630424e00c498e
baselbalkes <baselbalkes77@gmail.com>
2024-10-28 14:25:23 +0300

    test(fix api GET params)

--- src/app/api/news/[id]/route.ts
+++ src/app/api/news/[id]/route.ts
@@ -1,14 +1,21 @@
-import { sql } from "@vercel/postgres";  
-import { NextRequest, NextResponse } from "next/server";  
+import { sql } from "@vercel/postgres";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextRequest, { params }: { params: { id: string } }) {  
-    try {  
-        const fetchNewQuery = await sql`  
-        SELECT * FROM sportNews WHERE id = ${parseInt(params.id)}  
-        `;  
-        return NextResponse.json({ data: fetchNewQuery.rows[0] });  
-    } catch (error) {  
-        console.log(error);  
-        return NextResponse.json({ error: "فشل في تحميل الخبر" });  
-    }  
-}
+interface Params {  
+    id: string;  
+} 
+export async function GET( 
+    request: NextRequest, 
+    {params}: {params: Params}) {
+        const headers = request.headers;
+        headers.get("Content-Type");
+        try {
+            const fetchNewQuery = await sql `
+            SELECT * FROM sportNews WHERE id = ${parseInt(params.id)}
+            `;
+            return NextResponse.json({data: fetchNewQuery.rows[0]})
+        } catch ( error ) {
+            console.log( error );
+            return NextResponse.json({error: "فشل في تحميل الخبر"});
+        }
+    }
