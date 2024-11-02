ea644feb7f8d1335011578fd7c7c141e648f02f3
baselbalkes <baselbalkes77@gmail.com>
2024-11-02 12:06:03 +0300

    add fix match function

---
+++ src/apiFetching/matchesResults/addNewMatch.ts
@@ -0,0 +1,21 @@
+type ApiResponse = {
+    error?: string;
+    message?: string;
+};
+export const addNewMatch = async (formData: FormData): Promise<ApiResponse> => {
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL!;
+    try {
+        const apiFetch = await fetch(`${baseUrl}/api/matchesResults`, {
+            method: 'POST',
+            body: formData,
+        });
+        if(apiFetch.ok) {
+            const response: ApiResponse = await apiFetch.json();
+            return response;
+        }
+        return {error: "فشل في إضافـة مباراة جديدة!"}
+    } catch( error ) {
+        console.log( error );
+        return {error: "فشل الاتصــال!"}
+    }
+}
