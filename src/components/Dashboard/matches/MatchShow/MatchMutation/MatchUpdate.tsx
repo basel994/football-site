00677c15db9251837dee1b26469d9feaa97d83d9
baselbalkes <baselbalkes77@gmail.com>
2024-11-07 08:32:44 +0300

    fix(match update component types)

--- src/components/Dashboard/matches/MatchShow/MatchMutation/MatchUpdate.tsx
+++ src/components/Dashboard/matches/MatchShow/MatchMutation/MatchUpdate.tsx
@@ -3,7 +3,6 @@ import { updateMatch } from "@/apiFetching/matches/updateMatch";
 import styles from "./matchMutation.module.css";
 import CustomButton from "@/components/CustomButton/CustomButton";
 import CustomModal from "@/components/CustomModal/CustomModal";
-import TextInput from "@/components/Form/TextInput/TextInput";
 import { MatchType } from "@/types/matchType";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
@@ -43,29 +42,19 @@ export default function MatchUpdate({matcObject}: {matcObject: MatchType}) {
     }, []);
     const router = useRouter();
     const [visible, setVisible] = useState<boolean>(false);
-    const [ team_one, setTeam_one ] = useState<string>(matcObject.team_one);
-    const [ team_two, setTeam_two ] = useState<string>(matcObject.team_two);
-    const [teams, setTeams] = useState<{key: string, value: string}[] | null>(null);
+    const [ team_one, setTeam_one ] = useState<string>(String(matcObject.team_one));
+    const [ team_two, setTeam_two ] = useState<string>(String(matcObject.team_two));
+    const [teams, setTeams] = useState<{key: string, value: string | number}[] | null>(null);
     const [championships, setChampionships] = useState<{key: string, value: string}[] | null>(null);
     const [ championship, setChampionship ] = useState<string>(matcObject.championship);
-    const [ team_one_score, setTeam_one_score ] = useState<string>(String(matcObject.team_one_score));
-    const [ team_two_score, setTeam_two_score ] = useState<string>(String(matcObject.team_two_score));
     const [ match_date, setMatch_date ] = useState<string>(matcObject.match_date);
     const [ loading, setLoading ] = useState<boolean>(false);
     const [message, setMessage] = useState<string>("");
     const [error, setError] = useState<string>("");
     const modalBody = <div className={styles.modalBody}>
-        <SelectInput label="اختـر الفريـق الأول" value={team_one} options={teams} setValue={setTeam_one}/>
+        <SelectInput label="اختـر الفريـق الأول" value={team_one} options={teams} setValue={(setTeam_one)}/>
         <SelectInput label="اختـر الفريـق الثـاني" value={team_two} options={teams} setValue={setTeam_two}/>
         <SelectInput label="اختـر البطـولــة" value={championship} options={championships} setValue={setChampionship}/>
-        <TextInput label=" نتيجة المنتخب/الفريق الأول " 
-        type="text" 
-        value={team_one_score}
-        setState={setTeam_one_score}/>
-        <TextInput label="نتيجة المنتخب/الفريق الثاني" 
-        type="text" 
-        value={team_two_score}
-        setState={setTeam_two_score}/>
         <DateInput label="ادخل التاريخ والوقت" value={match_date} setValue={setMatch_date} />
     </div>
     const onOk = async () => {
@@ -75,11 +64,9 @@ export default function MatchUpdate({matcObject}: {matcObject: MatchType}) {
         else {
             setLoading(true);
             const formData = new FormData();
-            formData.append("team_one", team_one);
-            formData.append("team_two", team_two);
+            formData.append("team_one", String(team_one));
+            formData.append("team_two", String(team_two));
             formData.append("championship", championship);
-            formData.append("team_one_score", team_one_score);
-            formData.append("team_two_score", team_two_score);
             formData.append("match_date", match_date);
             const callAddFun = await updateMatch(String(matcObject.id), formData);
             if(callAddFun.error) {
@@ -99,17 +86,15 @@ export default function MatchUpdate({matcObject}: {matcObject: MatchType}) {
         setVisible(true);
         setError("");
         setMessage("");
-        setTeam_one(matcObject.team_one);
-        setTeam_two(matcObject.team_two);
+        setTeam_one(String(matcObject.team_one));
+        setTeam_two(String(matcObject.team_two));
         setChampionship(matcObject.championship);
-        setTeam_one_score(String(matcObject.team_one_score));
-        setTeam_two_score(String(matcObject.team_two_score));
         setMatch_date(matcObject.match_date);
     }
     return(
         <>
             <CustomButton title="تعديـل" 
-            bg="rgb(8, 92, 92)" 
+            bg="rgb(59, 87, 148)" 
             color="white" 
             clicked={updateClicked}/>
             <CustomModal visible={visible} 
