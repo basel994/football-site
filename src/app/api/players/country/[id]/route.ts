e2458935ca49c997620d2f3a5f8c2c65c468fe80
baselbalkes <baselbalkes77@gmail.com>
2024-11-13 09:18:23 +0300

    add api for fetching players by their countries

---
+++ src/app/api/players/country/[id]/route.ts
@@ -0,0 +1,18 @@
+import { sql } from "@vercel/postgres";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest, {params}: {params: Promise<{id: string}>}) {
+    const id = (await params).id;
+    const intId = parseInt(id);
+    const headers = request.headers;
+    headers.get("Content-Type");
+    try {
+        const getPlayersQuery = await sql `
+        SELECT * FROM players WHERE country = ${intId}
+        `;
+            return NextResponse.json({data: getPlayersQuery.rows});
+    } catch(error) {
+        console.log(error);
+        return NextResponse.json({error: "فشل تحميل لاعبو الفريق!"});
+    }
+}
