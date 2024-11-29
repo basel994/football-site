52d8b41dc31c741486c1f208c841d6aeba715fed
baselbalkes <baselbalkes77@gmail.com>
2024-11-29 11:32:28 +0300

    improve error style and fix user details displaying

--- src/components/Users/UserPage/UserPage.tsx
+++ src/components/Users/UserPage/UserPage.tsx
@@ -1,12 +1,13 @@
 import { userById } from "@/apiFetching/users/userById";
 import styles from "./userPage.module.css";
+import UserDetails from "./UserDetails/UserDetails";
 export default async function UserPage({id}: {id: number}) {
     const getUser = await userById(id);
     return(
         <div className={styles.container}>
             {
-                !getUser.data ? <p>{getUser.error}</p> : 
-                <div><p>{getUser.data.name}</p></div>
+                !getUser.data ? <p className={styles.error}>{getUser.error}</p> : 
+                <UserDetails userData={getUser.data} />
             }
         </div>
     );
