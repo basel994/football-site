1f889e5d42e840375a7547e604239dc9636cadd0
baselbalkes <baselbalkes77@gmail.com>
2024-11-29 14:46:10 +0300

    add apis for updating users names, last names and passwords

---
+++ src/app/api/auth/users/[id]/password/route.ts
@@ -0,0 +1,24 @@
+import { sql } from "@vercel/postgres";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PATCH(request: NextRequest, { params }: { params: Promise<{ id: string }> }) {  
+    const id = (await params).id;   
+    const newPassword = await request.json();
+    try {
+          const res = await sql`  
+          UPDATE users
+          SET password = ${newPassword}
+          WHERE id = ${parseInt(id)}  
+          RETURNING * 
+          `;  
+          if (res.rows.length === 0) {  
+            return NextResponse.json({ error: "المستخدم غـير موجود!" }, { status: 404 });  
+          }  
+          return NextResponse.json({ data: res.rows[0] }, { status: 200 });             
+ 
+
+    } catch (error) {  
+        console.error(error);  
+        return NextResponse.json({ error: "فشل في تعـديل كلمة المرور" }, { status: 500 });  
+    }  
+}
