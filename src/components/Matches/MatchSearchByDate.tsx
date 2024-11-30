bcb443c4719d16108edcb3c43eacec99bad795b2
baselbalkes <baselbalkes77@gmail.com>
2024-11-30 13:18:06 +0300

    add date search for matches

---
+++ src/components/Matches/MatchSearchByDate.tsx
@@ -0,0 +1,71 @@
+"use client";
+import { useEffect, useState } from "react";
+import DateInput from "../Form/DateInput/DateInput";
+import styles from "./matches.module.css";
+import { fetchFrontendMatches } from "@/apiFetching/matches/fetchFrontendMatches";
+import { FrontMatchType } from "@/types/frontMatchType";
+import Link from "next/link";
+import Match from "./Match/Match";
+
+interface ChampionGroup {  
+    champion: string;  
+    matches: FrontMatchType[];  
+}  
+const groupMatchesByChampion = (matches: FrontMatchType[]): ChampionGroup[] => {  
+    const championMap: { [key: string]: FrontMatchType[] } = {};  
+
+    matches.forEach(match => {  
+        if (!championMap[match.championship]) {  
+            championMap[match.championship] = [];  
+        }  
+        championMap[match.championship].push(match);  
+    });  
+
+    return Object.keys(championMap).map(champion => ({  
+        champion,  
+        matches: championMap[champion]  
+    }));  
+}; 
+export default function MatchSearchByDate() {
+    const [date, setDate] = useState<string>(new Date().toDateString());
+    const [matches, setMatches] = useState<FrontMatchType[] | null>(null);
+    const [error, setError] = useState<string>("");
+    useEffect( () => {
+        const getMatchesByDate = async() => {
+            const callFun = await fetchFrontendMatches(date);
+            if(callFun.data) {
+                setMatches(callFun.data);
+            }
+            else if(callFun.error) {
+                setError(callFun.error);
+            }
+        }
+        getMatchesByDate();
+    }, [date] );
+    return(
+        <>
+        <DateInput label="حدد تاريخ" 
+        setValue={setDate} 
+        value={date} />
+                    {
+                !matches? <p className={styles.error}>{error}</p> :
+                groupMatchesByChampion(matches).map((match, index) => {
+                    return(
+                        <div className={styles.championContainer} key={index}>
+                            <div className={styles.champion}><p>{match.champion}</p></div>
+                            {
+                                match.matches.map((match) => {
+                                    return(
+                                        <Link href={`/matches/${match.id}`} key={match.id}>
+                                            <Match matchObject={match} />
+                                        </Link> 
+                                    );
+                                })
+                            }
+                        </div>
+                    );
+                })
+            }
+        </>
+    );
+}
--- src/components/Matches/Matches.tsx
+++ src/components/Matches/Matches.tsx
@@ -1,53 +1,10 @@
 import styles from "./matches.module.css";
-import Match from "./Match/Match";
-import { fetchFrontendMatches } from "@/apiFetching/matches/fetchFrontendMatches";
-import { FrontMatchType } from "@/types/frontMatchType";
-import Link from "next/link";
-interface ChampionGroup {  
-    champion: string;  
-    matches: FrontMatchType[];  
-}  
-const groupMatchesByChampion = (matches: FrontMatchType[]): ChampionGroup[] => {  
-    const championMap: { [key: string]: FrontMatchType[] } = {};  
+import MatchSearchByDate from "./MatchSearchByDate";
 
-    matches.forEach(match => {  
-        if (!championMap[match.championship]) {  
-            championMap[match.championship] = [];  
-        }  
-        championMap[match.championship].push(match);  
-    });  
-
-    return Object.keys(championMap).map(champion => ({  
-        champion,  
-        matches: championMap[champion]  
-    }));  
-}; 
 export default async function Matches() {
-    const date = new Date();
-    const today = date.toDateString();
-    const getTodayMatches = await fetchFrontendMatches(today);
     return(
         <div className={styles.container}>
-
-            {
-                !getTodayMatches.data ? <p className={styles.error}>{getTodayMatches.error}</p> :
-                groupMatchesByChampion(getTodayMatches.data).map((match, index) => {
-                    return(
-                        <div className={styles.championContainer} key={index}>
-                            <div className={styles.champion}><p>{match.champion}</p></div>
-                            {
-                                match.matches.map((match) => {
-                                    return(
-                                        <Link href={`/matches/${match.id}`} key={match.id}>
-                                            <Match matchObject={match} />
-                                        </Link> 
-                                    );
-                                })
-                            }
-                        </div>
-                    );
-                })
-            }
+            <MatchSearchByDate />
         </div>
     );
 }
