a9f9959b474b630e5095c372548867b66cb76b37
baselbalkes <baselbalkes77@gmail.com>
2024-12-02 09:54:37 +0300

    fix(fetch players api response)

--- src/apiFetching/players/fetchFrontPlayers.ts
+++ src/apiFetching/players/fetchFrontPlayers.ts
@@ -4,18 +4,21 @@ type ApiResponse = {
     error?: string;
     data?: FrontPlayerType[] | [];
 };
-export const fetchFrontendPlayers = async (player: string): Promise<ApiResponse> => {
+export const fetchFrontendPlayers = async (player?: string): Promise<ApiResponse> => {
     const baseUrl = process.env.NEXT_PUBLIC_BASE_URL!;
     const urlFetched = `${baseUrl}/api/players/front?player=${player}`;
     try {
-        const apiFetch = await fetch(urlFetched, {cache: "no-store"});
-        if(apiFetch.ok) {
-            const response: ApiResponse = await apiFetch.json();
-            return response;
+        if(player) {
+            const apiFetch = await fetch(urlFetched, {cache: "no-store"});
+            if(apiFetch.ok) {
+                const response: ApiResponse = await apiFetch.json();
+                return response;
+            }
+            else {
+                return {error: "فشل في تحميـل اللاعبين!"}
+            }
         }
-        else {
-            return {error: "فشل في تحميـل اللاعبين!"}
-        }
+        else return {data: []};
     } catch( error ) {
         console.log( error );
         return {error: "فشل الاتصــال!"}
